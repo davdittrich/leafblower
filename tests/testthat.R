e22ab69fc38e5c6b0b5d95923f205ac126a3f3f5
Dennis Alexis Valin Dittrich <dd+github@dr-dittrich.de>
2026-04-18 14:05:37 +0200

    feat(scaffold): add package skeleton with DESCRIPTION, NAMESPACE, Makevars.in

---
+++ tests/testthat.R
@@ -0,0 +1,7 @@
+# This file is part of the R package leafblower.
+# See the file LICENSE in the package root for license information.
+
+library(testthat)
+library(leafblower)
+
+test_check("leafblower")
